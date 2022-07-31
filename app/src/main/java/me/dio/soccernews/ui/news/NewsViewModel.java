db0fe83d122ced4ea9d9ad887bb90aa6127872f5
RKamada <rafael-kamada2007@hotmail.com>
2022-07-31 11:57:12 -0300

    Adicionado uso de Retrofit e Picasso

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,5 +1,7 @@
 package me.dio.soccernews.ui.news;
 
+import android.widget.Toast;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,22 +9,52 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import me.dio.soccernews.ui.adapter.NewsAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://rafaelkamada.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // TODO remover mocks de notícias;
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviária Tem Desfalque Importante","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.."));
-        news.add(new News("Ferroviária Joga no Sábado","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.."));
-        news.add(new News("Copa do Mundo Feminina Está Terminando","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.."));
+        api = retrofit.create(SoccerNewsApi.class);
+        findNews();
+    }
 
-        this.news.setValue(news);
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()){
+                    news.setValue(response.body());
+                    NewsAdapter adapter = new NewsAdapter();
+                } else {
+                    ShowMessageError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                ShowMessageError();
+            }
+        });
+    }
+
+    private void ShowMessageError() {
+        //TODO criar alerta de erro
     }
 
     public LiveData<List<News>> getNews() {
