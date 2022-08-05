d09661ab5afe12fbc0a6fcf4164d3c7bbb85a195
RKamada <rafael-kamada2007@hotmail.com>
2022-08-04 23:07:33 -0300

    regularizações finais

---
+++ app/src/main/java/me/dio/soccernews/data/SoccerNewsRepository.java
@@ -0,0 +1,45 @@
+package me.dio.soccernews.data;
+
+import androidx.room.Room;
+
+import me.dio.soccernews.App;
+import me.dio.soccernews.data.local.SoccerNewsDb;
+import me.dio.soccernews.data.remote.SoccerNewsApi;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SoccerNewsRepository {
+
+    //region Constantes
+    private static final String REMOTE_API_URL = "https://rafaelkamada.github.io/soccer-news-api/";
+    private static final String LOCAL_DB_NAME = "soccer-news";
+    //endregion
+
+    //region Atributos
+    private SoccerNewsApi remoteApi;
+    private SoccerNewsDb localDb;
+
+    public SoccerNewsApi getRemoteApi() { return remoteApi; }
+    public SoccerNewsDb getLocalDb() { return localDb; }
+    //endregion
+
+    //region Singleton: garante uma instância única dos atributos relacionados ao Retrofit e Room.
+    public SoccerNewsRepository() {
+        remoteApi = new Retrofit.Builder()
+                .baseUrl(REMOTE_API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(SoccerNewsApi.class);
+
+        localDb = Room.databaseBuilder(App.getInstance(), SoccerNewsDb.class, LOCAL_DB_NAME)
+                .allowMainThreadQueries() //TODO fazer as devidas atualização para não utilizar isto.
+                .build();
+    }
+
+    public static  SoccerNewsRepository getInstance() { return LazyHolder.INSTANCE; }
+
+    private static class LazyHolder {
+        private static final SoccerNewsRepository INSTANCE = new SoccerNewsRepository();
+    }
+    //endregion
+}
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,18 +1,19 @@
 package me.dio.soccernews.ui.news;
 
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
-import me.dio.soccernews.data.remote.SoccerNewsApi;
+import me.dio.soccernews.data.SoccerNewsRepository;
 import me.dio.soccernews.domain.News;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
@@ -21,40 +22,37 @@ public class NewsViewModel extends ViewModel {
     }
 
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
-    private final MutableLiveData<State> state = new MutableLiveData<State>();
-    private final SoccerNewsApi api;
+    private final MutableLiveData<State> state = new MutableLiveData<>();
 
     public NewsViewModel() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://rafaelkamada.github.io/soccer-news-api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        api = retrofit.create(SoccerNewsApi.class);
-
         findNews();
     }
 
-    private void findNews() {
+    public void findNews() {
         state.setValue(State.DOING);
-        api.getNews().enqueue(new Callback<List<News>>() {
+
+        SoccerNewsRepository.getInstance().getRemoteApi().getNews().enqueue(new Callback<List<News>>() {
             @Override
-            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
                 if (response.isSuccessful()){
                     news.setValue(response.body());
                     state.setValue(State.DONE);
-                   // NewsAdapter adapter = new NewsAdapter();
                 } else {
                     state.setValue(State.ERROR);
                 }
             }
 
             @Override
-            public void onFailure(Call<List<News>> call, Throwable t) {
-                t.printStackTrace();
+            public void onFailure(@NonNull Call<List<News>> call, @NonNull Throwable error) {
+                // TODO remover printStackTrace quando subir em produção.
+                error.printStackTrace();
                 state.setValue(State.ERROR);
             }
         });
+    }
+
+    public void saveNews(News news) {
+        AsyncTask.execute(() -> SoccerNewsRepository.getInstance().getLocalDb().newsDao().save(news));
     }
 
     public LiveData<List<News>> getNews() {
